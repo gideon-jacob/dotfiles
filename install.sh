8c5f04d15e8c0ea0a1d006f5dce17ebe9c93e433
gideon-jacob <gideonjacob.in@gmail.com>
2025-02-01 04:04:51 +0530

    Added ngrok

--- install.sh
+++ install.sh
@@ -34,3 +34,11 @@ mv ~/.zshrc.bak ~/.zshrc
 # update theme
 sed -i '/^ZSH_THEME/c\ZSH_THEME="robbyrussell"' ~/.zshrc
 ########################################################################################################################
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+  | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+  && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+  | sudo tee /etc/apt/sources.list.d/ngrok.list \
+  && sudo apt update \
+  && sudo apt install ngrok
+
+########################################################################################################################
